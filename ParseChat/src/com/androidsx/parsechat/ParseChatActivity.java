ccbd4bb40a78c924a9f48e9b047e1b8a7dc3c354
Vicent Pérez <vicennt@gmail.com>
2014-04-02 21:28:50 +0200

    Create table message

--- ParseChat/src/com/androidsx/parsechat/ParseChatActivity.java
+++ ParseChat/src/com/androidsx/parsechat/ParseChatActivity.java
@@ -3,11 +3,20 @@ package com.androidsx.parsechat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.androidsx.hellowordparse.R;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseChatActivity extends Activity {
+	
+	private EditText txtMessage;
+	private Button btnSend;
+	private ParseObject messages;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,32 @@ public class ParseChatActivity extends Activity {
 
 		Parse.initialize(this, "mMjR5lvou6mzMhjymYbEh39RCsqGQkvNLQqDQ47u",
 				"d8rT5X0HVKSS297euA4koJgsAdJaG1HEIlYnvgPM");
-
+		
+		createTableMessage();
 	}
-
+	
+	public void createTableMessage(){
+		messages = new ParseObject("Messages");
+		messages.saveInBackground();
+	}
+	
+	public void setupUI(){
+		txtMessage = (EditText)findViewById(R.id.etMensaje);
+		btnSend = (Button)findViewById(R.id.btnSend);
+		btnSend.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String message =  txtMessage.getText().toString();
+				
+				messages.put("userName", "Vicent");
+				messages.put("message", message);
+				messages.saveInBackground();
+				
+			}
+		});
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
