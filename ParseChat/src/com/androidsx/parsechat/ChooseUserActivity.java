a610b0f137aa5df2a11e6fa4ced1a180229ae6c5
Vicent Pérez <vicennt@gmail.com>
2014-04-03 14:21:01 +0200

    Create methods getParseUsers and mountSpinnerUsers

--- ParseChat/src/com/androidsx/parsechat/ChooseUserActivity.java
+++ ParseChat/src/com/androidsx/parsechat/ChooseUserActivity.java
@@ -1,76 +1,121 @@
 package com.androidsx.parsechat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.androidsx.hellowordparse.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 public class ChooseUserActivity extends Activity {
 
 	private Button btnLogin;
-	private EditText userName;
-	private EditText userPassword;
+	private Spinner spinnerUsers;
+	private boolean checkLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choose_user);
 		setupUI();
+		getParseUsers();
 	}
-	
-	private void setupUI(){
-		userName = (EditText)findViewById(R.id.loginUsername);
-		userPassword = (EditText)findViewById(R.id.loginpass);
-		btnLogin = (Button) findViewById(R.id.buttonLogin);	
+
+	private void setupUI() {
+		spinnerUsers = (Spinner) findViewById(R.id.spinnerUsers);
+		btnLogin = (Button) findViewById(R.id.buttonLogin);
 		btnLogin.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				boolean loginResult = checkLogin();
-				
-				if (loginResult){
-					openParseChatActivity(v);
-				}else{
-					
+
+				String name = spinnerUsers.getSelectedItem().toString();
+
+				boolean loginResult = checkLogin(name);
+
+				if (loginResult) {
+					showToast("Welcome user!");
+					openParseChatActivity(v, name);
+				} else {
+					showToast("Invalid username");
+					openParseChatActivity(v, name);
 				}
 
 			}
 		});
-		
+
 	}
 
+
+	public void openParseChatActivity(View view, String name) {
+		Intent i = new Intent(this, ParseChatActivity.class);
+		i.putExtra("user", name);
+		startActivity(i);
+	}
+	
+	public void getParseUsers(){
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Users");
+		query.findInBackground(new FindCallback<ParseObject>() {
+			public void done(List<ParseObject> scoreList, ParseException e) {
+				if (scoreList != null) {
+					ArrayList<String> nameUsers = new ArrayList<String>();
+					for	(int i = 0; i < scoreList.size(); i++){
+						nameUsers.add(scoreList.get(i).getString("username"));					
+					}	
+					mountSpinnerUsers(nameUsers);			
+				} 
+			}
+		});		
+	}
+	
+	private void mountSpinnerUsers(ArrayList<String> names){
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, names);
+		spinnerUsers.setAdapter(adapter);
+	}
+
+	public boolean checkLogin(String name) {
+
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Messages");
+		query.whereEqualTo("userName", name);
+		query.findInBackground(new FindCallback<ParseObject>() {
+			public void done(List<ParseObject> scoreList, ParseException e) {
+				if (scoreList != null) {
+					checkLogin = true;
+				} else {
+					checkLogin = false;
+				}
+			}
+		});
+
+		return checkLogin;
+	}
+	
+
+
+	public void showToast(String text) {
+		Toast toast = Toast.makeText(this, text, 3000);
+		toast.show();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.choose_user, menu);
 		return true;
-	}
-	
-	public void openParseChatActivity(View view){
-		Intent i = new Intent(this , ParseChatActivity.class);
-		
-		i.putExtra("user", userName.getText().toString());
-		startActivity(i);
-	}
-	
-	public boolean checkLogin(){
-		
-		String name = userName.getText().toString(); 
-		String password = userPassword.getText().toString();
-		
-		// Create a query - name and pass check
-		ParseQuery<ParseObject> query = ParseQuery.getQuery("GameScore");
-		query.getInBackground(name, null);
-	
-		return false;
 	}
 
 }
